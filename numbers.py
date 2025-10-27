9d9f931c194e0629838b2364c2de5a889c0084c5
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-10-28 00:39:07 +0530

    second commit

---
+++ numbers.py
@@ -0,0 +1,11 @@
+age = 35  #INTEGER
+PI = 3.14159  #FLOAT
+
+maths_operation = 1 + 3 * 4 /2 - 2 
+print(maths_operation)
+
+float_division = 12 / 3
+print(float_division)
+
+integer_division = 12//3 # // -> removes every element after decimal
+print(integer_division)
