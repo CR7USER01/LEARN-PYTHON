7d4525c1c01b36a5a6bed222912980cab091d6d1
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-10-28 21:14:24 +0530

    fourth commit

---
+++ StringsInPython.py
@@ -0,0 +1,31 @@
+my_string = "hewllo, world!"
+#print(my_string)
+
+string_with_quots = 'Hello, it'
+#print(string_with_quots)
+
+
+string_with_quots = "Hello, it's me."
+another_with_quotes = "he said \"you are amazing!\" yesterday"
+
+multiline = """hello, world. my name is jitu . i am a boy."""
+#print(multiline)
+
+
+age = 21
+age_as_str = str(age)
+#print("you are " + age_as_str)
+
+age = 21
+age_as_string = str(age)
+#print("you are " + age_as_string)
+
+#
+
+name ="jitu"
+print(f"hello,{name}")
+
+
+
+
+
