f1a671dbc92640a880e8503d9c7c7885073e7982
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-10-31 08:07:31 +0530

    sixth commit

---
+++ gettingUserInput.py
@@ -0,0 +1,18 @@
+# my_name = "jitu"
+# your_name = input("enter your name: ")
+
+# print (f"hello {your_name}. my name is {my_name }.")
+
+# age = input("Enter your age : ")
+# age_num = int(age)
+# print (f"you have live for {age * 12} months")
+# print (f"you have live for {age * 12} months")
+
+age = int(input("Enter your age: "))
+months = age * 12
+print(f"you have lived for {months} months.")
+
+
+
+
+
