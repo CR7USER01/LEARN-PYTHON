5392194cf22cbee45538cbef4a0d9a6284a2ea0a
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-11-03 01:37:20 +0530

    seventh commit

---
+++ andOrkeyword.py
@@ -0,0 +1,24 @@
+"""age = int(input("enter:"))
+#age = 25 
+result = age < 18 and age <65# true and true
+
+print(result)"""
+
+"""default_age = 30
+age = 0
+
+user_age = age or default_age
+print("user_age")"""
+
+"""age = int(input("enter: "))
+result = age > 18 or age
+print(result)"""
+
+default_greeting = "there"
+name = input("enter your name: (optional) ")
+
+user_name = name or default_greeting
+print (f"hello,{user_name}!")
+
+
+
--- gettingUserInput.py
+++ gettingUserInput.py
@@ -2,9 +2,10 @@
 # your_name = input("enter your name: ")
 # print (f"hello {your_name}. my name is {my_name }.")
 
- age = input("Enter your age : ")
- age_num = int(age)
- print (f"you have live for {age * 12} months")
+age = input("Enter your age: ")
+age_num = int(age)
+print(f"You have lived for {age_num * 12} months.")
+
 #  print (f"you have live for {age * 12} months")
 
 # age = int(input("Enter your age: "))
