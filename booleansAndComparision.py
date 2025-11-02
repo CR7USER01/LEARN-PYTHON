9e797005174ed4cfd3f964c490f4aaf76c6950cc
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-11-03 00:45:04 +0530

    sixth commit

---
+++ booleansAndComparision.py
@@ -0,0 +1,29 @@
+# truthy = True
+# false = False
+
+"""age = 20
+is_over_age = age <= 18
+print(is_over_age )
+is_under_age = age < 18
+is_twenty = age == 20  #single equal to (=)assignment .
+                       #double equAL TO (==) """
+
+
+"""my_number = 5
+
+user_number = int(input("Enter a number: "))
+
+matches = my_number == user_number
+
+print(f"you got it right : {matches}.")"""
+
+my_number = 9
+user_number = int(input("enter a number:"))
+
+if  (user_number == my_number):
+    print("its right.")
+
+else:
+
+ print("its wrong ")
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+my_number = 5
+user_number = int(input("enter a number:"))
+
+if my_number==user_number 
+print("l")
+
+else:
+
+print("try again")
