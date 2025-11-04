9836568437f35865389bbb7e9a263b98d464d342
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-11-05 02:49:00 +0530

    eleventh commit

---
+++ advanceSets.py
@@ -0,0 +1,22 @@
+"""art_friends = {"rolf","Anne","jen"}
+science_friends = {"jen", "charlie",}
+
+art_but_not_science = art_friends.difference(science_friends)
+
+science_friend_but_not_art = science_friends.difference(art_friends)
+
+not_in_both = art_friends.symmetric_difference(science_friends)
+
+all_friends = art_friends.union(science_friends)
+
+art_and_science = art_friends.intersection(science_friends)
+print(art_but_not_science)
+print(science_friend_but_not_art)
+print(not_in_both)
+print(art_and_science)
+print(all_friends)#set cant contain duplicates"""
+
+
+
+
+
