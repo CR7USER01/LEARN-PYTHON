33328a993c743b0f60cd9f9df8b34719d5652dc0
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-11-05 00:29:01 +0530

    tenth commit

---
+++ SetsInPython.py
@@ -0,0 +1,9 @@
+art_friends = {"Rolf","Anne"}
+science_friends ={"jen", "charlie"}
+
+art_friends.add("jen")
+print(art_friends)
+
+#art_friends.add("jen")
+art_friends.remove("jen")
+print(art_friends)
