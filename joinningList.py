ba1b3d855bf06bc5c6041baf6ef32c3ccc9559b0
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-11-07 03:57:33 +0530

    fifteenth commiit

---
+++ joinningList.py
@@ -0,0 +1,7 @@
+friends = ["Jitu","abaiswa","Anil","Sudu"]
+print(f"My friends are {friends}.")
+
+friends = ["Jitu","abaiswa","Anil","Sudu"]
+comma_separated ="," .join(friends)
+print(f"My friends are {comma_separated}.")
+
