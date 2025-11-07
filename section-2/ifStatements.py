304582e1784b497da8d468dcbad6868c586cb67f
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-11-07 21:24:41 +0530

    sevsnteenth commit

---
+++ section-2/ifStatements.py
@@ -0,0 +1,47 @@
+
+
+"""friend = "Rolf"
+user_name = input("Enter your name: ")
+
+if user_name == friend:
+    print("hello , friend!")
+    print("this runs too.")
+else:
+ print("this runs anyway.")"""
+
+"""name = input("Enter your name: ")
+
+print(bool(name))
+
+if name:
+ print("we know your name.")"""
+
+friends = ["Rolf", "Bob", "Anne"]
+family = ["cj","vk"]
+
+user_name = input("Enter your name: ")
+
+"""if user_name in friends:  #
+    print("hi we got this ")
+else:
+    print("not yet")"""
+
+"""if user_name in family:  #
+    print("hi we got this ")
+else:
+    print("no i dont know you ")"""
+
+if user_name in friends:  #
+    print("hi we got this ")
+
+elif user_name in family:  
+    print("hi we got this too family  ")
+else:
+    print("no i dont know you ")
+
+
+
+
+
+
+
