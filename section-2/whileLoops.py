93184ca30f488546f622214ecb4951d456a28dc5
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-11-08 01:21:41 +0530

    eighteenth commit

---
+++ section-2/whileLoops.py
@@ -0,0 +1,26 @@
+"""
+is_learning = True
+
+while is_learning:
+
+    print("you are learning!")
+    user_input = input("Are you still learning?")
+    is_learning = user_input == "yes"""
+
+"""user_input = input("Do y ouwish to run the program? (yes/no): ")
+
+while user_input == "Yes":
+    print("We are running!")
+    user_input = input("Do you wish to run the program?(Yes/no): ")
+
+print("We stopped running. ")"""
+
+count = 1
+
+while count <= 5:
+    print("Number:", count)
+    count += 1  # increase count by 1
+
+
+
+
