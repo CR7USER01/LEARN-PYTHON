39704897ac5783890c59c4cfc81051434a49d6b3
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-11-10 01:44:16 +0530

    nineteenth commit

--- SetsInPython.py
+++ SetsInPython.py
@@ -5,5 +5,9 @@ art_friends.add("jen")
 print(art_friends)
 
 #art_friends.add("jen")
-art_friends.remove("jen")
+#art_friends.remove("jen")
+art_friends.pop()
+print(art_friends)
+
+art_friends.pop()
 print(art_friends)
