d63e2e3f0d13a1908802475469fddfc5f851ab48
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-11-12 02:51:50 +0530

    24-th commit

---
+++ section-2/listSllicin.py
@@ -0,0 +1,10 @@
+friends = ["Rolf", "Charlie", "Anna", "Bob", "Jen"]
+
+print(friends[2:4])
+print(friends[:4])
+print(friends[1:3])
+print(friends[-3:4])
+print(friends[:-3])
+print(friends[-3:])
+print(friends[-2:])
+print(friends[:-2])
