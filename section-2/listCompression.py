ecef4f6023dcd9f1e9c92ea5da82356450160e2d
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-11-12 19:12:02 +0530

    26-th commit

---
+++ section-2/listCompression.py
@@ -0,0 +1,30 @@
+#numbers = [0, 1, 2, 3, 4]
+"""doubled_numbers = [number * 2 for number in range(5) ]
+print(doubled_numbers)
+
+friend_ages = [22,31,35,37]
+age_strings= [f"My friend is {age} years old." for age in friend_ages]
+print (age_strings)
+
+names =["Rolf", "Bob", "Jen"]
+lower = [name.lower() for name in names]
+print(lower)"""
+friend = input("Enter your name: ")
+friends =["Rolf", "Bob", "Jen", "Charlie", "Anne"]
+friends_lower = [name.lower() for name in friends]
+print (friends_lower)
+
+if friend in friends:
+    friend_titlecased = friend.title()
+    print(f"{friend.titlecased} is one of your friends.")
+
+
+
+
+
+               
+
+
+
+
+    
