792c38ca9eeb80789237caabae2501d70f94ad23
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-11-21 23:33:29 +0530

    36-th commit

---
+++ Section-3/tempCodeRunnerFile.py
@@ -0,0 +1,53 @@
+movies = []
+
+
+def add_movie():
+    title = input("Enter the movie title: ")
+    director = input("Enter the movie director: ")
+    year = input("Enter the movir release year: ")
+
+
+    movies.append({
+        'title': title,
+        'director': director,
+        'year': year
+    })
+
+
+def show_movies():
+    for movie in movies:
+        print_movie(movie)
+
+def print_movie(movie):
+    print(f"Title: {movie['title']}") 
+    print(f"Director: {movie['director']}")
+    print(f"Release year: {movie['year']}")
+
+
+
+
+def find_movies():
+    search_title = input("Enter movie title you're looking for: ")
+
+    for movie in movies:
+        if movie["title"] == search_title:
+            print_movie(movie)
+
+
+def menu():
+    selection = input(MENU_PROMPT)
+    while selection != 'q':
+        if selection == "a":
+            add_movie()
+        elif selection == "1":
+            show_movies
+        elif selection == "f":
+            find_movies()
+        else:
+            print('unknown command. Please try again.')
+
+        selection = input(MENU_PROMPT)
+
+
+menu()
+
