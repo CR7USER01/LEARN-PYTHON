24a70487d739539a1d02f5ef43f6a3f0bf2bd3c6
CR7USER01 <2201109118_cse@pmec.ac.in>
2026-01-22 14:27:07 +0530

    47th-commit

---
+++ section-11/browserHtml.py
@@ -0,0 +1,24 @@
+"""1️⃣ What happens when you open a website?
+When you open https://example.com:
+Browser downloads HTML
+Browser parses HTML
+Browser builds a DOM (tree)
+Browser displays the page
+This is called HTML parsing by the browser."""
+#-----------------------------------------------------------------------
+#----------------------------------------------------------------------
+"""2️⃣ What is HTML parsing?
+HTML parsing is the process of analyzing HTML code
+6️⃣ Why developers use Inspect (real life)
+
+Find tag names
+Find class / id
+Debug layout
+Use data for web scraping
+
+👉 This is how you know what to parse with BeautifulSoup
+
+7️⃣ Connecting browser → Python (important)
+Browser parses HTML → DOM
+BeautifulSoup parses HTML → soup object
+Both do the same understanding, just for different purposes."""
